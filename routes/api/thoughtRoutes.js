7df45aa7ea62a33fd2678754b16beeec53c5d8f3
Adam-Smart <adams8632@hotmail.co.uk>
2023-09-03 18:40:29 +0100

    Added comments to routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,13 +8,13 @@ const {
   addReaction,
   removeReaction
 } = require('../../controllers/thoughtController');
-
+// Route to GET all thoughts and to POST a thought
 router.route('/').get(getThoughts).post(createThought);
-
+// Route to GET, PUT and DELETE a thought by id 
 router.route('/:id').get(getOneThoughtById).put(updateThought).delete(deleteThought);
-
+//Route to POST a reaction to a thought
 router.route('/:thoughtId/reactions').post(addReaction);
-
+// Route to DELETE a reaction for a thought 
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,12 +8,13 @@ const {
   addFriend,
   removeFriend
 } = require('../../controllers/userController');
-
+// Route to GET and all Users and to POST a new user
 router.route('/').get(getUsers).post(createUser);
-
+// Route To GET, PUT and DELETE a user by id
 router.route('/:id').get(getOneUserById).put(updateUser).delete(deleteUser);
-
+//Route to POST a new friend to a user by id 
 router.route('/:id/friends/:friendId').post(addFriend);
+// Route to DELETE a friend from a user by id
 router.route('/:id/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
